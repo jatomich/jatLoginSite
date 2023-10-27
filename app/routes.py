d6d86a6749878de4a8c0f1dfdbd14355a3109e8d
jatomich <jatomic@spareparse.com>
2023-10-27 06:33:26 -0500

    Frontend

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,7 @@
-from flask import render_template, current_app as app
+from flask import (render_template,
+                   request,
+                   jsonify,
+                   current_app as app)
 from app import db
 
 
@@ -6,3 +9,13 @@ from app import db
 @app.route("/index")
 def index():
     return render_template("index.html")
+
+
+@app.route("/hello", methods=['POST'])
+def hello():
+    # This gets the JSON data from the React request
+    data = request.get_json()
+    # This gets the name from the data
+    name = data.get('name')
+    # This returns a JSON response with a greeting message
+    return jsonify({'message': f'Hello, {name}!'})
