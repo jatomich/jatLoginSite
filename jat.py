ae1047d52e80b034c5aad91f34cbfeca849c930c
jatomich <jatomic@spareparse.com>
2023-10-27 05:07:03 -0500

    local entry

---
+++ jat.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from app import create_app, db
+from app.models import User
+
+app = create_app()
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User}
