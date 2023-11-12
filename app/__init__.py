7a4634dad044fe540ad589daab829cc78dfabcce
jatomich <jatomic@spareparse.com>
2023-11-12 00:10:56 -0600

    Add Movie and TvShow tables. Redesign directory structure.

--- app/__init__.py
+++ app/__init__.py
@@ -1,53 +1,31 @@
 from flask import Flask
-from config import Config, basedir
-from flask_sqlalchemy import SQLAlchemy
+from config import Config, instance_path
 from flask_bootstrap import Bootstrap
-from flask_cors import CORS
-# from flask_restful import Api
+from flask_restful import Api
+# from .models import NetflixMovies, NetflixShows
+from main import models, routes
 
-db = SQLAlchemy()
-# api = Api()
+# db = SQLAlchemy()
+api = Api()
 bootstrap = Bootstrap()
-cors = CORS()
+
+movie_list = (movies := models.Movie.query) if models.Movie.query else []
+show_list = (shows := models.Show.query) if models.Show.query else []
 
 
 def create_app(config_class=Config):
     app = Flask(__name__,
-                instance_path=basedir + "/app",
-                instance_relative_config=True,
-                static_url_path=basedir + "/app/static",
-                static_folder="/static")
+                instance_path=instance_path,
+                instance_relative_config=True)
                 
     app.config.from_object(config_class)
     app.config.from_envvar('CUSTOM_CONFIG')
 
     db.init_app(app)
-    # api.init_app(app)
+    api.init_app(app)
     bootstrap.init_app(app)
-    cors.init_app(app)
 
     with app.app_context():
-        from . import routes, models
         db.create_all()
-        # url_map = app.url_map
-        # try:
-        #     for rule in url_map.iter_rules('static'):
-        #         url_map._rules.remove(rule)
-        # except ValueError:
-        #     # no static view yet created
-        #     pass
-
-        # # register new; the same function is used
-        # app.add_url_rule(
-        #     app.static_url_path + '/<path:filename>',
-        #     endpoint='static', view_func=app.send_static_file
-        # )
-        # movies = routes.NetflixMovies()
-        # shows = routes.NetflixShows()
-        # movies_api = movies.make_api({"key": "value"})
-        # shows_api = shows.make_api({"key": "value"})
-        # api.add_resource(movies_api, '/netflix_movies')
-        # api.add_resource(shows_api, '/netflix_shows')
-
+       
     return app
-
--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 import os
+from pathlib import Path
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(os.environ.get('LOC_ANCHOR', '__file__')))
+instance_path = basedir + '/app'
 
 
 class Config():
     SECRET_KEY = '123abc'
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{basedir}/" + "app.db"
+
+    # static_url_path=str(instance_path) + '/static',
+    #             static_folder='./static',
+    #             template_folder='./templates'
