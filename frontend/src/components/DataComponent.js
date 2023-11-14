3db04182e38ac8990fba417fb3ae1baf5bc5aca8
jatomich <jatomic@spareparse.com>
2023-11-14 03:27:10 -0600

    Fix data retrieval function in frontend.

--- frontend/src/components/DataComponent.js
+++ frontend/src/components/DataComponent.js
@@ -1,52 +1,156 @@
+// import React from 'react';
+// import instance from '../axios';
+
+// import Container from 'react-bootstrap/Container';
+// import '../styles/App.css';
+
+// class NetflixData extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       movies: [],
+//       shows: [],
+//     };
+//   }
+
+//   componentDidMount() {
+//     this.fetchMovies();
+//     this.fetchShows();
+//   }
+
+//   fetchMovies = async () => {
+//     try {
+//       const response = await instance.get(`/netflix_movies`, {});
+//       this.setState({ movies: response.data.movies });
+//     } catch (error) {
+//       console.error("Error fetching movies: ", error);
+//     }
+//   };
+
+//   fetchShows = async () => {
+//     try {
+//       const response = await instance.get(`/netflix_shows`, {});
+//       this.setState({ shows: response.data.shows });
+//     } catch (error) {
+//       console.error("Error fetching shows: ", error);
+//     }
+//   };
+
+//   render() {
+//     const { movies, shows } = this.state;
+
+//     return (
+//     <Container>
+//       <h1 id="movie-type" className="netflix-type">Movies</h1>
+//       {movies?.map((movie, index) => (
+//         <div className='netflix-div' key={index}>
+//           <h2>{movie.title}</h2>
+//           <p><b>cast:</b> {movie.cast}</p>
+//           <p><b>date added:</b> {movie.date_added}</p>
+//           <p><b>rating:</b> {movie.rating}</p>
+//           <p><b>listed_in:</b> {movie.listed_in}</p>
+//           <p><b>director:</b> {movie.director}</p>
+//           <p><b>show_id:</b> {movie.show_id}</p>
+//           <p><b>format:</b> {movie.format}</p>
+//           <p><b>country:</b> {movie.country}</p>
+//           <p><b>release_year:</b> {movie.release_year}</p>
+//           <p><b>duration:</b> {movie.duration}</p>
+//           <p><b>description:</b> {movie.description}</p>
+//         </div>
+//       ))}
+
+//       <h1 id="show-type" className="netflix-type">TV Shows</h1>
+//       {shows?.map((show, index) => (
+//         <div className='netflix-div' key={index}>
+//           <h2>{show.title}</h2>
+//           <p><b>cast:</b> {show.cast}</p>
+//           <p><b>date added:</b> {show.date_added}</p>
+//           <p><b>rating:</b> {show.rating}</p>
+//           <p><b>listed_in:</b> {show.listed_in}</p>
+//           <p><b>director:</b> {show.director}</p>
+//           <p><b>show_id:</b> {show.show_id}</p>
+//           <p><b>format:</b> {show.format}</p>
+//           <p><b>country:</b> {show.country}</p>
+//           <p><b>release_year:</b> {show.release_year}</p>
+//           <p><b>duration:</b> {show.duration}</p>
+//           <p><b>description:</b> {show.description}</p>
+//         </div>
+//       ))}
+//     </Container>
+//   );
+//   }
+// }
+
+// export default NetflixData;
+
 import React, { useState, useEffect } from 'react';
-import axios from '../axios';
+import instance from '../axios';
 
 import Container from 'react-bootstrap/Container';
 import '../styles/App.css';
-
 
 function NetflixData() {
   const [movies, setMovies] = useState([]);
   const [shows, setShows] = useState([]);
 
   useEffect(() => {
-    const fetchMovies = async () => {
-      const response = await axios.get(`/netflix_movies`, {})
-      setMovies(response.data.movies);
-    };
-
-    const fetchShows = async () => {
-      const response = await axios.get(`/netflix_shows`, {})
-      setShows(response.data.shows);
-    };
-
     fetchMovies();
     fetchShows();
   }, []);
 
+  const fetchMovies = async () => {
+    try {
+      const response = await instance.get(`/netflix_movies`, {});
+      setMovies(response.data.movies);
+    } catch (error) {
+      console.error("Error fetching movies: ", error);
+    }
+  };
+
+  const fetchShows = async () => {
+    try {
+      const response = await instance.get(`/netflix_shows`, {});
+      setShows(response.data.shows);
+    } catch (error) {
+      console.error("Error fetching shows: ", error);
+    }
+  };
+
   return (
     <Container>
       <h1 id="movie-type" className="netflix-type">Movies</h1>
-      {movies.map((movie, index) => (
+      {movies?.map((movie, index) => (
         <div className='netflix-div' key={index}>
           <h2>{movie.title}</h2>
           <p><b>cast:</b> {movie.cast}</p>
           <p><b>date added:</b> {movie.date_added}</p>
+          <p><b>rating:</b> {movie.rating}</p>
+          <p><b>listed_in:</b> {movie.listed_in}</p>
           <p><b>director:</b> {movie.director}</p>
-          <p><b>cast:</b> {movie.cast}</p>
+          <p><b>show_id:</b> {movie.show_id}</p>
+          <p><b>format:</b> {movie.format}</p>
+          <p><b>country:</b> {movie.country}</p>
+          <p><b>release_year:</b> {movie.release_year}</p>
+          <p><b>duration:</b> {movie.duration}</p>
           <p><b>description:</b> {movie.description}</p>
-          <p><b>movie id:</b> {movie.show_id}</p>
         </div>
       ))}
 
-      <h1 id="show-type" className="netflix-type">Shows</h1>
-      {shows.map((show, index) => (
+      <h1 id="show-type" className="netflix-type">TV Shows</h1>
+      {shows?.map((show, index) => (
         <div className='netflix-div' key={index}>
           <h2>{show.title}</h2>
-          <p><b>director:</b> {show.director}</p>
           <p><b>cast:</b> {show.cast}</p>
+          <p><b>date added:</b> {show.date_added}</p>
+          <p><b>rating:</b> {show.rating}</p>
+          <p><b>listed_in:</b> {show.listed_in}</p>
+          <p><b>director:</b> {show.director}</p>
+          <p><b>show_id:</b> {show.show_id}</p>
+          <p><b>format:</b> {show.format}</p>
+          <p><b>country:</b> {show.country}</p>
+          <p><b>release_year:</b> {show.release_year}</p>
+          <p><b>duration:</b> {show.duration}</p>
           <p><b>description:</b> {show.description}</p>
-          <p><b>show id:</b> {show.show_id}</p>
         </div>
       ))}
     </Container>
@@ -54,3 +158,47 @@ function NetflixData() {
 }
 
 export default NetflixData;
+
+
+//   return (
+//     <Container>
+//       <h1 id="movie-type" className="netflix-type">Movies</h1>
+//       {movies?.map((movie, index) => (
+//         <div className='netflix-div' key={index}>
+//           <h2>{movie.title}</h2>
+//           <p><b>cast:</b> {movie.cast}</p>
+//           <p><b>date added:</b> {movie.date_added}</p>
+//           <p><b>rating:</b> {movie.rating}</p>
+//           <p><b>listed_in:</b> {movie.listed_in}</p>
+//           <p><b>director:</b> {movie.director}</p>
+//           <p><b>show_id:</b> {movie.show_id}</p>
+//           <p><b>format:</b> {movie.format}</p>
+//           <p><b>country:</b> {movie.country}</p>
+//           <p><b>release_year:</b> {movie.release_year}</p>
+//           <p><b>duration:</b> {movie.duration}</p>
+//           <p><b>description:</b> {movie.description}</p>
+//         </div>
+//       ))}
+
+//       <h1 id="show-type" className="netflix-type">TV Shows</h1>
+//       {shows?.map((show, index) => (
+//         <div className='netflix-div' key={index}>
+//           <h2>{show.title}</h2>
+//           <p><b>cast:</b> {show.cast}</p>
+//           <p><b>date added:</b> {show.date_added}</p>
+//           <p><b>rating:</b> {show.rating}</p>
+//           <p><b>listed_in:</b> {show.listed_in}</p>
+//           <p><b>director:</b> {show.director}</p>
+//           <p><b>show_id:</b> {show.show_id}</p>
+//           <p><b>format:</b> {show.format}</p>
+//           <p><b>country:</b> {show.country}</p>
+//           <p><b>release_year:</b> {show.release_year}</p>
+//           <p><b>duration:</b> {show.duration}</p>
+//           <p><b>description:</b> {show.description}</p>
+//         </div>
+//       ))}
+//     </Container>
+//   );
+// }
+
+// export default NetflixData;
--- jat.py
+++ jat.py
@@ -2,13 +2,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from config import Config, basedir
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, jsonify
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import pandas as pd
 from flask_restful import Api, Resource
-
 
 ###########################
 # INSTANTIATE APPLICATION #
@@ -72,8 +71,13 @@ CORS(app)
 ########################### 
 # INSTANTIATE RESTFUL API #
 ###########################
-api = Api(app)
+class NetflixShows(Resource):
+    def get(self):
+        # columns = ["title", "director", "cast", "description", "show_id"]
+        shows = [{k: v for k, v in list(s.__dict__.items())[1:]} for s in NetflixMedia.query if s.format.lower() == 'tv show']
+        return shows
 
+api = Api(app)
 #####################################
 # BOOTSTRAP FOR FRONTEND FORMATTING #
 #####################################
@@ -124,19 +128,138 @@ api.add_resource(NetflixMovies, '/api/netflix_movies')
     #     cls.response = response
     #     return cls
 
-class NetflixShows(Resource):
 # @app.route("/netflix_shows")
 # def netflix_shows():
 #     columns = ["title", "director", "cast", "description", "show_id"]
 #     shows = [{k: v for k, v in s.__dict__.items() if k in columns} for s in TVShow.query]
 #     return {'shows': shows}
+###########################
+# INSTANTIATE APPLICATION #
+###########################
+app = Flask(__name__,
+            instance_path=basedir,
+            instance_relative_config=True,
+            static_url_path=str(basedir) + '/static',
+            static_folder='./static',
+            template_folder='./templates'
+            )
+app.config.from_object(Config)
+app.config.from_envvar('CUSTOM_CONFIG')
+
+################################# 
+# CROSS-ORIGIN RESOURCE SHARING #
+#################################
+CORS(app)
+
+#####################################
+# BOOTSTRAP FOR FRONTEND FORMATTING #
+#####################################
+Bootstrap(app)
+
+######################
+# CONFIGURE DATABASE #
+######################
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+
+    def add_new_user(username, email):
+        new_user = User(username=username, email=email)
+        db.session.add(new_user)
+        db.session.commit()
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+
+class NetflixMedia(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    show_id = db.Column(db.String(24), index=True, unique=True)
+    format = db.Column(db.String(16))
+    title = db.Column(db.String(120))
+    director = db.Column(db.String(120))
+    cast = db.Column(db.String(256))
+    country = db.Column(db.String(64))
+    date_added = db.Column(db.String(16))
+    release_year = db.Column(db.Integer)
+    rating = db.Column(db.String(8))
+    duration = db.Column(db.Integer)
+    listed_in = db.Column(db.String(256))
+    description = db.Column(db.String(512))
+
+    def __repr__(self):
+        return f"<Netflix Media: {self.title}; {self.format}>"
+    
+with app.app_context():
+    db.create_all()
+
+########################### 
+# INSTANTIATE RESTFUL API #
+###########################
+class NetflixMovies(Resource):
 
     def get(self):
         # columns = ["title", "director", "cast", "description", "show_id"]
-        shows = [{k: v for k, v in list(s.__dict__.items())[1:]} for s in NetflixMedia.query if s.format.lower() == 'tv show']
-        return shows
+        movies = [{k: v for k, v in list(s.__dict__.items())[1:]} for s in NetflixMedia.query if s.format.lower() == 'movie']
+        return jsonify(kwargs=movies)
     
+class NetflixShows(Resource):
+    
+    def get(self):
+        # columns = ["title", "director", "cast", "description", "show_id"]
+        shows = [{k: v for k, v in list(s.__dict__.items())[1:]} for s in NetflixMedia.query if s.format.lower() == 'tv show']
+        return jsonify(kwargs=shows)
+
+api = Api(app)
+
+api.add_resource(NetflixMovies, '/api/netflix_movies')
 api.add_resource(NetflixShows, '/api/netflix_shows')
+
+
+##################
+# VIEW FUNCTIONS #
+##################
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/api/netflixdf')
+def netflixdf():
+    netflix_raw_df = pd.read_csv(url_for('static', filename='input/csv/netflix_titles.csv')).rename(columns={'type': 'format'})
+
+    netflix_df = netflix_raw_df.dropna().sort_values(
+        ['format', 'title']
+        ).reset_index(drop=True).copy()
+
+    # print(netflix_df.iloc[:10, 0:3])
+
+    for row in netflix_df.iterrows():
+        new_item = NetflixMedia(**row[1])
+        print(new_item)
+        db.session.add(new_item)
+        db.session.commit()
+
+    return redirect(url_for('index'))
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+
+
+
+
+# @app.route("/netflix_movies")
+# def netflix_movies():
+#     columns = ["title", "director", "cast", "description", "show_id"]
+#     movies = [{k: v for k, v in m.__dict__.items() if k in columns} for m in Movie.query]
+#     return {'movies': movies}
 
     # @classmethod
     # def make_api(cls, response):
@@ -144,12 +267,11 @@ api.add_resource(NetflixShows, '/api/netflix_shows')
     #     return cls
 
 
-# @app.route("/imdb/<endpoint>'", methods=["GET", "POST"])
-# def imdb_data(endpoint):
-#     imdb_df = pd.read_csv('static/output/imdb.csv',
-#                           low_memory=False)
-#     print(imdb_df)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+# instance = axios.create({
+#     baseURL: 'http://localhost:5000',
+#     timeout: 1000,
+#     headers: {'X-Custom-Header': 'foobar'}
+# });
